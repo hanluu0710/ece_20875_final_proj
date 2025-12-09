cc371fb87480ed4af4354332cf299a46d0dfcc08
Mỹ Hân Lưu <luum@purdue.edu>
2025-12-08 21:44:58 -0500

    q2

--- MiniProjectPath2.py
+++ MiniProjectPath2.py
@@ -22,6 +22,8 @@ dataset_2['Precipitation']         = pandas.to_numeric(dataset_2['Precipitation'
 x_samples = [dataset_2["Brooklyn Bridge"], dataset_2['Manhattan Bridge'], dataset_2['Queensboro Bridge'], dataset_2['Williamsburg Bridge']]
 x_labels = ["Brooklyn Bridge", 'Manhattan Bridge', 'Queensboro Bridge', 'Williamsburg Bridge']
 
+
+
 def getRelevant():
     global x_samples
     scores = []
@@ -35,6 +37,30 @@ def getRelevant():
     print(scores)
     print(f"Sensor should not be on {x_labels[scores.index(min(scores))]}")
 
+def weather (dataset_2):
+    dataset_2["Total"]  = pandas.to_numeric(dataset_2["Total"].replace(',','', regex=True))
+    dataset_2["Precipitation"]  = pandas.to_numeric(dataset_2["Precipitation"].replace(',','', regex=True))
+    dataset_2["Low Temp"]  = pandas.to_numeric(dataset_2["Low Temp"].replace(',','', regex=True))
+    dataset_2["High Temp"]  = pandas.to_numeric(dataset_2["High Temp"].replace(',','', regex=True))
+
+
+    X = dataset_2[["High Temp", "Low Temp", "Precipitation"]]
+    y = dataset_2["Total"]
+
+
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LinearRegression
+    from sklearn.metrics import r2_score
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, shuffle=False
+    )
+
+    model = LinearRegression().fit(X_train, y_train)
+    preds = model.predict(X_test)
+
+    print("R2:", r2_score(y_test, preds))
 
 
 getRelevant()
+weather(dataset_2)
